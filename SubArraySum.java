3913a2936a4ce1614023fd4533ed4313c970c152
sanat <sanatnri@gmail.com>
2023-02-24 03:36:52 +0530

    modified

--- SubArraySum.java
+++ SubArraySum.java
@@ -1,19 +1,55 @@
 package arrayprogramm;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import javafx.scene.shape.Arc;
+
 public class SubArraySum {
+    public static ArrayList<Integer> subArraySum(int []arr,int n,int target){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int start = 0 , end = 0;
+        int currSum = 0;
+        if(target == 0){
+            list.add(-1);
+            return list;
+        }
+        
+        while(start < n)
+        {
+          currSum +=  arr[start];
+          if(currSum > target)
+          {
+              while(currSum > target)
+              {
+                currSum -=  arr[end];
+                end = end + 1;
+              }
+          }
+          
+          
+          if(currSum == target)
+          {
+              list.add(end+1);
+              list.add(start+1);
+              return list;
+          }
+          
+          
+         if(currSum < target)
+          {
+            start ++;  
+          }
+         
+        }
+        list.add(-1);
+        return list;
+    }     
     public static void main(String[] args) {
         int arr[]={1,2,3,7,5};
+        int n=arr.length;
         int sum=12;
-        for(int i=1;i<arr.length;i++){
-            int ans=arr[i];
-            for(int j=i+1;j<arr.length;j++)
-            { 
-                ans+=arr[j];
-                if( ans==sum)
-                {
-                    System.out.println(i+" "+j);
-                }
-            }
-        }
+        subArraySum(arr,n, sum);
+        
     }
 }
